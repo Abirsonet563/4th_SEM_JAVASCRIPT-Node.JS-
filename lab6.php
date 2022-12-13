e239b0796b5d01a86be3fe63ea213e82e60a3b46
Abir Hasan <81650568+Abirsonet563@users.noreply.github.com>
2022-12-13 12:17:44 +0800

    Create lab6.php

---
+++ lab6.php
@@ -0,0 +1,65 @@
+<?php
+/*$f = date ("D");
+echo "$f";
+if ($f == "Tue")
+{
+    echo "True";
+}
+elseif ($f == "True")
+{
+    echo "Ok";
+}
+else
+{
+    echo "Lol";
+}
+*/
+/*
+$x = 10;
+
+switch ($x)
+{
+    case 1: echo "1";
+}*/
+/*
+$i = 1;
+while ($i <= 5)
+{
+    echo $i.'<br>';
+    $i++;
+}
+*/
+/*
+$i = 1;
+do 
+{
+    $i++;
+    echo $i.'<br>';
+  
+}
+while ($i <= 10);
+*/
+
+
+/*
+$x = array("a","b","c");
+
+echo $x[1]."<br>";
+
+foreach ($x as $v)
+{
+    echo $v."<br>";
+}
+echo"<br><br>";
+for ($i = 0; $i<3; $i++)
+{
+    echo $x[$i]."<br>";
+}
+*/
+/*
+for ($i = 0; $i<= 10; $i++)
+{
+echo $i.'<br>';
+}
+*/
+?>
